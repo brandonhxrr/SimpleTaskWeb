6e3803e2843bff7a656a6a1cb62a08f5b46d44fc
Brandon Herrera <brandon.hernandez@encora.com>
2024-09-08 11:57:54 -0600

    Add TodoRequest to send parameters to filter and paginate the content

---
+++ backend/Simpletask/src/main/java/com/brandonhxrr/simpletask/model/TodoRequest.java
@@ -0,0 +1,14 @@
+package com.brandonhxrr.simpletask.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TodoRequest {
+    int page = 1;
+    String sortBy = "";
+    String taskStatus = "";
+    String taskName = "";
+    String taskPriority = "";
+}
